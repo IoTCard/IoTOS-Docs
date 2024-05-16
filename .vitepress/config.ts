2508ef963b38092c7cf07b0b56b84a29fd4a4ac6
iotos <card@iotos.top>
2024-05-16 10:30:15 +0800

    修改备案号

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -324,7 +324,7 @@ export default defineConfigWithTheme<ThemeConfig>({
 
     footer: {
       license: {
-        text: 'Apache-2.0 赣ICP备2023012091号-2',
+        text: '赣ICP备2023012091号-2',
         link: 'https://beian.miit.gov.cn'
       },
       copyright: `Copyright © 2023-${new Date().getFullYear()}  iotos.top`
